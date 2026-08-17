9ade7bbbafda537718b5436c99e28d31d92869e9
tropicaliguanodon158 <111194338+tropicaliguanodon158@users.noreply.github.com>
2026-08-17 21:09:54 +0300

    Добавление картинок к ответам бота

---
+++ app/config/media.py
@@ -0,0 +1,23 @@
+COMMAND_IMAGES = {
+    "start": None,
+    "help": None,
+    "profile": None,
+    "balance": None,
+    "shop": None,
+    "daily": None,
+    "hourly": None,
+    "level_up": None,
+    "rank_up": None,
+}
+
+INTERACTION_IMAGES = {
+    "hit": [
+        None,
+    ],
+    "kiss": [
+        None,
+    ],
+    "piss": [
+        None,
+    ],
+}
